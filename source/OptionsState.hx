2dde1be401e7350d3e99840ef66a08690631b78d
Angelfoxy <eliasbejesus@outlook.com>
2024-02-14 13:23:26 -0500

    options trans

--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -1,10 +1,18 @@
 package;
 
+import flixel.tweens.FlxTween;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 
+import GlobalVariables as Global;
+
 class OptionsState extends FlxState {
     var options:FlxSprite;
+
+    var dark:FlxSprite;
 
     override function create() {
         options = new FlxSprite(-50,10).loadGraphic(Paths.image('options'));
@@ -13,5 +21,38 @@ class OptionsState extends FlxState {
         add(options);
 
         super.create();
+
+		dark = new FlxSprite(0, 0);
+		dark.makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		dark.alpha = 1;
+		add(dark);
     }
+
+    override function update(elapsed:Float) {
+		if (dark.alpha > 0)
+		{
+			dark.alpha -= 0.05;
+		}
+		else if (dark.alpha == 0)
+		{
+			Global.dark = 0;
+		}
+        
+        super.update(elapsed);
+
+        if (FlxG.keys.justPressed.ESCAPE) {
+            Back();
+        }
+    }
+
+	function Back()
+	{
+		FlxTween.tween(dark, {alpha: 1}, 1, {ease: FlxEase.cubeIn, onComplete: switchA});
+		Global.dark = 1;
+	}
+
+	function switchA(t:FlxTween)
+	{
+		FlxG.switchState(new MainMenuState());
+	}
 }
