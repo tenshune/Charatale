b15be11ad7d2c20858f85b1c353e4cbfe7782bb4
Angelfoxy <eliasbejesus@outlook.com>
2024-02-14 00:00:52 -0500

    Reowrk

--- export/neko/haxe/ApplicationMain.hx
+++ export/neko/haxe/ApplicationMain.hx
@@ -35,7 +35,7 @@ class ApplicationMain
 		ManifestResources.init(config);
 		#end
 
-		app.meta["build"] = "11";
+		app.meta["build"] = "12";
 		app.meta["company"] = "Angelfoxiano";
 		app.meta["file"] = "chocolateDummy";
 		app.meta["name"] = "Charatale";
